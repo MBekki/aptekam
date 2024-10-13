d622876884a3954f08cdb1435cdc76b228e19ee8
Bekzod <bekzodmahammadov627@gmail.com>
2024-10-13 09:06:49 +0500

    about section

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,5 @@ export { default as NavbarTop } from './NavbarTop';
 export { default as Navbar } from './Navbar';
 export { default as Hero } from './Hero';
 export { default as Feature } from './Feature';
+export { default as AboutUs } from './AboutUs';
+export { default as Partnerts } from './Partnerts';
