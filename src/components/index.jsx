7ebadcc7408808f3f6ed80aefb07a322f289d33f
Bekzod <bekzodmahammadov627@gmail.com>
2024-10-13 09:44:35 +0500

    service

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,3 +4,4 @@ export { default as Hero } from './Hero';
 export { default as Feature } from './Feature';
 export { default as AboutUs } from './AboutUs';
 export { default as Partnerts } from './Partnerts';
+export { default as Service } from './Service';
